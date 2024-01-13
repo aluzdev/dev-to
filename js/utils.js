e17e3665501fb75ea497203394cde706f03de99d
aluzdev <aluzdev@gmail.com>
2024-01-13 06:37:35 -0700

    feat: fetches data as an array

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,10 @@ export const fetchAllData = async () => {
     const url = 'https://dev-to-fcbcc-default-rtdb.firebaseio.com/.json'
     const response = await fetch(url)
     const data = await response.json()
-    console.log({ data })
-    return data
+    const dataArray = Object.keys(data.posts).map((key) => ({
+        ...data.posts[key],
+        key,
+    }))
+    console.log({ dataArray })
+    return dataArray
 }
