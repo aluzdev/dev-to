7e1af8b9ca42028f915fc53844ffefbaf13309b0
aluzdev <aluzdev@gmail.com>
2024-01-13 21:20:49 -0700

    refactor: addPostToList function added to utils (#12)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 import { getAllPosts } from './utils.js'
-import { createPostElement } from './utils.js'
+import { addPostsToList } from './utils.js'
 
 const posts = await getAllPosts()
-const postsList = document.getElementById('postsList')
-const locationToInsertFirstPostImage = document.getElementById('firstPostImage')
-postsList.innerHTML = ''
 
-const addPostToList = (postData) => {
-    const firstPostImage = postData[0].imageValue
-    locationToInsertFirstPostImage.src = firstPostImage
-
-    postData.forEach((post) => {
-        const postElement = createPostElement(post)
-        postsList.appendChild(postElement)
-    })
-}
-
-addPostToList(posts)
+addPostsToList(posts)
