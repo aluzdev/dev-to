8f6f8baef336da9d805182ae2589829b569b6359
cesarmoreno <cesardelangelmoreno@gmail.com>
2024-01-14 19:51:41 -0600

    Add search in console

---
+++ js/search.js
@@ -0,0 +1,18 @@
+import { getAllPosts } from './utils.js';
+console.log("Si esta jalanhdo");
+const findPostByTitleValue = async () => {
+
+        let postsObject = await getAllPosts();
+        let searchInput = document.getElementById('search-input');
+        let title = searchInput.value.toLowerCase();
+
+        // Busca el primer post que cumple con el título
+        const foundPost = Object.values(postsObject).find((post) => post.titleValue.toLowerCase().includes(title));
+
+        if (foundPost) {
+            console.log('Post Found:', foundPost);
+        }
+};
+
+let searchInput = document.getElementById('search-input');
+searchInput.addEventListener('input', findPostByTitleValue);
