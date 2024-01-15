6f6c00ad7f85ccd861c8c34378cf15c368d48fd2
cesarmoreno <cesardelangelmoreno@gmail.com>
2024-01-14 19:51:41 -0600

    Add search in console

--- js/search.js
+++ js/search.js
@@ -1,23 +1,25 @@
-import { getAllPosts } from './utils.js';
-import {addPostsToList} from './utils.js';
+import { getAllPosts } from './utils.js'
+import { addPostsToList } from './utils.js'
 
-console.log("Si está jalando");
+console.log('Si está jalando')
 
 const findPostsByTitleValue = async () => {
-    let postsObject = await getAllPosts();
-    let searchInput = document.getElementById('search-input');
-    let title = searchInput.value.toLowerCase();
+    let postsObject = await getAllPosts()
+    let searchInput = document.getElementById('search-input')
+    let title = searchInput.value.toLowerCase()
 
     // Filtra todos los posts que cumplen con el título
-    const matchingPosts = Object.values(postsObject).filter((post) => post.titleValue.toLowerCase().includes(title));
+    const matchingPosts = Object.values(postsObject).filter((post) =>
+        post.titleValue.toLowerCase().includes(title)
+    )
 
     if (matchingPosts.length > 0) {
         //console.log('Posts Found:', matchingPosts);
-        addPostsToList(matchingPosts);
+        addPostsToList(matchingPosts)
     } else {
-        console.log('No matching posts found.');
+        console.log('No matching posts found.')
     }
-};
+}
 
-let searchInput = document.getElementById('search-input');
-searchInput.addEventListener('input', findPostsByTitleValue);
+let searchInput = document.getElementById('search-input')
+searchInput.addEventListener('input', findPostsByTitleValue)
