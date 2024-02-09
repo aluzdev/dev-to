f786046612956b7c3071ff2c25d487b3b842106f
aluz <aluzdev@gmail.com>
2024-02-09 12:35:05 -0600

    fixed getAllPosts function

--- js/utils.js
+++ js/utils.js
@@ -1,26 +1,21 @@
 const API_URL = 'https://dev-to-fcbcc-default-rtdb.firebaseio.com/.json'
-
+const backend_url = 'http://localhost:1337'
 export const getAllPosts = async () => {
     try {
-        const response = await fetch(API_URL)
+        const response = await fetch(`${backend_url}/posts`)
 
         if (!response.ok) {
             throw new Error(`Failed to fetch data. Status: ${response.status}`)
         }
 
         const data = await response.json()
+        console.log({ data })
 
-        if (!data || !data.posts) {
+        if (!data) {
             throw new Error('Invalid data format received from the API.')
         }
 
-        const allPosts = Object.keys(data.posts).map((id) => ({
-            ...data.posts[id],
-            id,
-        }))
-        // console.log({ allPosts })
-
-        return allPosts
+        return data
     } catch (error) {
         // Handle errors gracefully, e.g., log or throw a custom error
         console.error('Error fetching data:', error)
